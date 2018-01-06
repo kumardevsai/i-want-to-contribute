ef36e486bb152b6ec3acd00cb6de81733f77b576
Thomas Baird <tbaird@mail.umw.edu>
2018-01-06 11:11:21 -0500

    Continuing to es6-ify server files

--- src/server/index.js
+++ src/server/index.js
@@ -7,6 +7,6 @@ colors.enabled = true; // enables colors so they show in foreman console
 import App from './app';
 
 const PORT = process.env.PORT || 5000;
-App.listen(PORT, function () {
+App.listen(PORT, () => {
   console.log(`Running on port ${PORT}`.green)
 });
